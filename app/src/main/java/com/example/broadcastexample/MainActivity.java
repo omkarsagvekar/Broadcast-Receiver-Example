6723c8eb9c4fb9af21cb0e2a2f8d41b35bafeb35
Omkar <omkar.s@xploresense.com>
2024-06-27 10:03:20 +0530

    Added onStart and onStop method to restrict the app to accept receivers when app is closed.

--- app/src/main/java/com/example/broadcastexample/MainActivity.java
+++ app/src/main/java/com/example/broadcastexample/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.broadcastexample.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     View view;
+    BroadcastExample broadcastExample = new BroadcastExample();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +25,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         view =binding.getRoot();
         setContentView(view);
-        BroadcastExample broadcastExample = new BroadcastExample();
-        registerReceiver(broadcastExample, new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
+
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(broadcastExample, new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastExample);
     }
 }
